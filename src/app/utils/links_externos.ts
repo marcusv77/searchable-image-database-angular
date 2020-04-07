c412a821b3166feecec7df44431f2811fd18c96f
Raniere Silva <raniere@rgaiacs.com>
2020-04-07 13:53:36 -0300

    Improve list of universities at foot bar
    Close https://github.com/CRICDatabase/searchable-image-database-angular/issues/2

--- src/app/utils/links_externos.ts
+++ src/app/utils/links_externos.ts
@@ -1,7 +1,4 @@
 export enum LinksExternos {
-    UNIVERSIDADE_FEDERAL_OURO_PRETO = "https://www.ufop.br/",
-    DEPARTAMENTO_COMPUTACAO_UFOP = "http://www3.decom.ufop.br/decom/inicio/",
-    ESCOLA_FARMACIA_UFOP = "https://escoladefarmacia.ufop.br/",
-    PARCEIRO_BERKELEY = "https://www.berkeley.edu/",
-    FACEBOOK = "https://www.facebook.com/cricdatabase",
+    UFOP = "https://www.ufop.br/",
+    UFC = "http://www.ufc.br/",
 }
