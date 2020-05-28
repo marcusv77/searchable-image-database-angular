199e1b068d9b45cefb08b6f977fb648e887833d9
Raniere Silva <raniere@rgaiacs.com>
2020-05-28 17:52:55 -0300

    Remove legacy code related with sign up

--- src/app/services/login/sign_up.service.ts
+++ src/app/services/login/sign_up.service.ts
@@ -1,13 +1,8 @@
-import { Injectable, EventEmitter, OnInit, OnDestroy } from "@angular/core";
-import { Router } from "@angular/router";
+import { Injectable, OnInit, OnDestroy } from "@angular/core";
 
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
 
 import { UsuarioService } from "../usuarios/usuarios.service";
-import { ChavesArmazenamentoBrowser } from "../../utils/chaves_armazenamento_browser";
-import { UsuarioAutenticacaoModelRequisicao } from "src/app/models/autenticacao/usuario_autenticacao.model";
-import { ArmazenamentoBrowser } from "src/app/utils/browser_storage/browser_storage";
 
 
 @Injectable({
@@ -16,14 +11,7 @@ import { ArmazenamentoBrowser } from "src/app/utils/browser_storage/browser_stor
 
 export class SignUpService implements OnInit, OnDestroy {
 
-    private usuarioBanco: UsuarioAutenticacaoModelRequisicao;
-    private usuarioAutenticado = false;
-    public usuarioLogadoEventEmitter = new EventEmitter<boolean>();
-    private armazenamentoBrowser: ArmazenamentoBrowser;
-
-    constructor(private router: Router, private usuarioService: UsuarioService) {
-        this.armazenamentoBrowser = new ArmazenamentoBrowser();
-    }
+    constructor(private usuarioService: UsuarioService) { }
 
     ngOnInit() { }
 
@@ -35,25 +23,5 @@ export class SignUpService implements OnInit, OnDestroy {
         return this.usuarioService.sign_up(
             data
         );
-        // .pipe(
-        //     map(
-        //         (retorno) => {
-        //             this.usuarioBanco = retorno;
-        //             this.usuarioAutenticado = true;
-        //             this.usuarioLogadoEventEmitter.emit(true);
-
-        //             const usuarioLogado = {
-        //                 id_usuario: this.usuarioBanco.usuario.id,
-        //                 email: this.usuarioBanco.usuario.email,
-        //                 token_autenticacao: this.usuarioBanco.token_autenticacao
-        //             };
-
-        //             this.armazenamentoBrowser.armazenarDadoSessao(ChavesArmazenamentoBrowser.CHAVE_USUARIO_LOGADO, usuarioLogado);
-        //             this.router.navigate([""]);
-
-        //             return true;
-        //         }
-        //     )
-        // );
     }
 }
