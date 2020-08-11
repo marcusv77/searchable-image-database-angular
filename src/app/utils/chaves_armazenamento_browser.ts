dfe6bd532817b508d8d09555c7a83623424f1e69
Fagner de Oliveira Bernardo <bernardo.fagner@hotmail.com>
2020-08-10 22:57:47 -0300

    ...

--- src/app/utils/chaves_armazenamento_browser.ts
+++ src/app/utils/chaves_armazenamento_browser.ts
@@ -2,5 +2,5 @@ export enum ChavesArmazenamentoBrowser {
     
     CHAVE_USUARIO_LOGADO = 'usuario_logado',
     CHAVE_TODAS_LESOES = 'todas_lesoes',
-    CHAVE_TODAS_DESCRICOES = 'todas_descricoes'
+    CHAVE_TODAS_DESCRICOES = 'todas_descricoes',
 }
