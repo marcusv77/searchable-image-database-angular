6beab5219eb3671cf99cd41bf1fbbf538203643f
Raniere Silva <raniere@rgaiacs.com>
2020-09-12 21:55:10 -0300

    Fix contact details
    Close https://github.com/CRICDatabase/searchable-image-database-angular/issues/180

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
             $ENV: {
-                CRIC_EMAIL: JSON.stringify(process.env.CRIC_DOMAIN),
+                CRIC_EMAIL: JSON.stringify(process.env.CRIC_EMAIL),
                 CRIC_DOMAIN: JSON.stringify(process.env.CRIC_DOMAIN),
                 CRIC_API_DOMAIN: JSON.stringify(process.env.CRIC_API_DOMAIN),
                 CRIC_PLAYGROUND: JSON.stringify(process.env.CRIC_PLAYGROUND)
